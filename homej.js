05529f13318f9f460ea1ada0658e3188bdbe062c
NabilHilmi21 <nabilmhilmi21@gmail.com>
2024-11-24 18:22:42 +0700

    Add files via upload

---
+++ homej.js
@@ -0,0 +1,19 @@
+function openNavFoot() {
+  document.getElementById("navThings").style.display="block"
+  document.getElementById("navFooterTitle2").style.color="rgb(59, 130, 246)"
+}
+
+function closeNavFoot() {
+  document.getElementById("navThings").style.display="none"
+  document.getElementById("navFooterTitle2").style.color="rgb(255, 255, 255)"
+}
+
+function openRescFoot() {
+  document.getElementById("navThings2").style.display="block"
+  document.getElementById("rescFooterTitle2").style.color="rgb(59, 130, 246)"
+}
+
+function closeRescFoot() {
+  document.getElementById("navThings2").style.display="none"
+  document.getElementById("rescFooterTitle2").style.color="rgb(255, 255, 255)"
+}
---
+++ webdevproj.js
@@ -0,0 +1,23 @@
+function openBRP() {
+    document.getElementById("bojongRobotP").style.display = "block"
+    document.getElementById("viewmoreBR").style.display = "none"
+    document.getElementById("viewlessBR").style.display = "block"
+}
+
+function closeBRP() {
+    document.getElementById("bojongRobotP").style.display = "none"
+    document.getElementById("viewmoreBR").style.display = "block"
+    document.getElementById("viewlessBR").style.display = "none"
+}
+
+function openBCP() {
+    document.getElementById("bojongCodesP").style.display = "block"
+    document.getElementById("viewmoreBC").style.display = "none"
+    document.getElementById("viewlessBC").style.display = "block"
+}
+
+function closeBCP() {
+    document.getElementById("bojongCodesP").style.display = "none"
+    document.getElementById("viewmoreBC").style.display = "block"
+    document.getElementById("viewlessBC").style.display = "none"
+}
